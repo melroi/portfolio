7234dfbc8027dd158d3dd59ff14ae6cd7f39cea6
melroi <125811920+melroi@users.noreply.github.com>
2024-12-16 22:13:01 +0100

    aZQESRDTFGH

--- script.js
+++ script.js
@@ -8,24 +8,5 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-const themeToggle = document.getElementById('themeToggle');
-const closebtn = document.getElementById('closebtn');
-
-themeToggle.addEventListener('change', function() {
-  if (this.checked) {
-    // Mode sombre
-    document.body.style.backgroundColor = '#333';
-    document.body.style.color = '#fff';
-  } else {
-    // Mode clair
-    document.body.style.backgroundColor = '#fff';
-    document.body.style.color = '#000';
-  }
-});
-
-// Code pour fermer le menu (si nécessaire)
-closebtn.addEventListener('click', function() {
-  document.querySelector('.sidenav').style.width = '0';
-});
 
 
