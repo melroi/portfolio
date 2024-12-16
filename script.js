cb49e25ea2e1f1ca9cdb9958ff23613fdbcf823b
melroi <125811920+melroi@users.noreply.github.com>
2024-12-16 19:02:11 +0100

    aoezuhr

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+const themeToggle = document.getElementById('themeToggle');
+const closebtn = document.getElementById('closebtn');
+
+themeToggle.addEventListener('change', function() {
+  if (this.checked) {
+    // Mode sombre
+    document.body.style.backgroundColor = '#333';
+    document.body.style.color = '#fff';
+  } else {
+    // Mode clair
+    document.body.style.backgroundColor = '#fff';
+    document.body.style.color = '#000';
+  }
+});
+
+// Code pour fermer le menu (si nécessaire)
+closebtn.addEventListener('click', function() {
+  document.querySelector('.sidenav').style.width = '0';
+});
+
+
